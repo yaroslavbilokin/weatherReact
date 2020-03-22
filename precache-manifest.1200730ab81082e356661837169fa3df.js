c80cd8d29d5c7e7b44b1e865cc6a46c39e80306f
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-03-22 22:09:45 +0200

    Updates

---
+++ precache-manifest.1200730ab81082e356661837169fa3df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1f6436b9940158e53e3d768cb35b39",
+    "url": "/weatherReact/index.html"
+  },
+  {
+    "revision": "f30fe16e154a9020a90b",
+    "url": "/weatherReact/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "31f9a467ccac2e03782b",
+    "url": "/weatherReact/static/css/main.1b26ab75.chunk.css"
+  },
+  {
+    "revision": "f30fe16e154a9020a90b",
+    "url": "/weatherReact/static/js/2.9674f9a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherReact/static/js/2.9674f9a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f9a467ccac2e03782b",
+    "url": "/weatherReact/static/js/main.f9391a90.chunk.js"
+  },
+  {
+    "revision": "0f07d9e5ef1569479e20",
+    "url": "/weatherReact/static/js/runtime-main.274edf81.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/weatherReact/static/media/background.cc54c19b.jpg"
+  }
+]);
